f78207c71cdf773c5f5b07e7f6ce5501a6531729
Millieeen <emillinelam@gmail.com>
2025-04-01 02:24:10 +0200

    CSS recherche d'utilisateurs

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -1,46 +1,48 @@
 @extends('layouts.barreNav')
 
 @section('content')
-<div class="container">
-    <h1>Rechercher des utilisateurs</h1>
+<div class="recherche-users">
+    <div class="container">
+        <h1>Rechercher des utilisateurs</h1>
 
-    <form method="GET" action="{{ route('users.index') }}" class="mb-4">
-        <div class="row">
-            <div class="col">
-                <input type="text" name="prenom" class="form-control" placeholder="Prénom" value="{{ request('prenom') }}">
+        <form method="GET" action="{{ route('users.index') }}" class="mb-4">
+            <div class="row">
+                <div class="col">
+                    <input type="text" name="prenom" class="form-control" placeholder="Prénom" value="{{ request('prenom') }}">
+                </div>
+                <div class="col">
+                    <input type="text" name="name" class="form-control" placeholder="Nom" value="{{ request('name') }}">
+                </div>
+                <div class="col">
+                    <input type="text" name="adresse" class="form-control" placeholder="Adresse" value="{{ request('adresse') }}">
+                </div>
+                <div class="col">
+                    <input type="number" name="age" class="form-control" placeholder="Âge" value="{{ request('age') }}">
+                </div>
+                <div class="col">
+                    <input type="text" name="metier" class="form-control" placeholder="Métier" value="{{ request('metier') }}">
+                </div>
+                <div class="col">
+                    <button type="submit" class="btn btn-primary">Rechercher</button>
+                    <a href="{{ route('users.index') }}" class="btn btn-secondary">Réinitialiser</a>
+                </div>
             </div>
-            <div class="col">
-                <input type="text" name="name" class="form-control" placeholder="Nom" value="{{ request('name') }}">
-            </div>
-            <div class="col">
-                <input type="text" name="adresse" class="form-control" placeholder="Adresse" value="{{ request('adresse') }}">
-            </div>
-            <div class="col">
-                <input type="number" name="age" class="form-control" placeholder="Âge" value="{{ request('age') }}">
-            </div>
-            <div class="col">
-                <input type="text" name="metier" class="form-control" placeholder="Métier" value="{{ request('metier') }}">
-            </div>
-            <div class="col">
-                <button type="submit" class="btn btn-primary">Rechercher</button>
-                <a href="{{ route('users.index') }}" class="btn btn-secondary">Réinitialiser</a>
-            </div>
-        </div>
-    </form>
+        </form>
 
-    <h2>Résultats :</h2>
-    <ul class="list-group">
-        @foreach($users as $user)
-            <li class="list-group-item">
-                <strong>{{ $user->prenom }} {{ $user->name }}</strong> - {{ $user->metier }}
-                <br>📍 {{ $user->adresse }} | 🎂 {{ $user->age }} ans
-                <br>
-                <a href="{{ route('users.show', $user->id) }}" class="btn btn-info btn-sm">Voir Profil</a>
-            </li>
-        @endforeach
-    </ul>
+        <h2>Résultats :</h2>
+        <ul class="list-group">
+            @foreach($users as $user)
+                <li class="list-group-item">
+                    <strong>{{ $user->prenom }} {{ $user->name }}</strong> - {{ $user->metier }}
+                    <br>📍 {{ $user->adresse }} | 🎂 {{ $user->age }} ans
+                    <br>
+                    <a href="{{ route('users.show', $user->id) }}" class="btn btn-info btn-sm">Voir Profil</a>
+                </li>
+            @endforeach
+        </ul>
 
 
-    {{ $users->links() }}
+        {{ $users->links() }}
+    </div>
 </div>
 @endsection
